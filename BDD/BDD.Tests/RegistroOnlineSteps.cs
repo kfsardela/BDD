f9fbb5323810598bf23c527268d7942801b69d21
Rafael Cruz <rcruz@mundipagg.com>
2016-03-15 18:16:53 -0300

    Create UnitTests, SpecFlow and More

---
+++ BDD/BDD.Tests/RegistroOnlineSteps.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace BDD.Tests
+{
+    [Binding]
+    public class RegistroOnlineSteps
+    {
+        IWebDriver Browser;
+
+        [BeforeScenario]
+        public void CreateWebDriver()
+        {
+            //Cria a instancia do browser antes de executar os cenarios
+            this.Browser = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void CloseWebDriver()
+        {
+            //Fecha o browser depois que termina os cenarios
+            this.Browser.Close();
+            this.Browser.Dispose();
+        }
+
+
+        [Given(@"que sou um novo usuário")]
+        public void DadoQueSouUmNovoUsuario()
+        {
+            //Faz Nada
+        }
+        
+        [When(@"o eu navegar para a página de cadastro")]
+        public void QuandoOEuNavegarParaAPaginaDeCadastro()
+        {
+            //Navega para a URL da pagina de Cadastro
+            this.Browser.Navigate().GoToUrl("http://localhost:23324/Account/Create");
+        }
+        
+        [When(@"inseri todas as informações do formulário corretas")]
+        public void QuandoInseriTodasAsInformacoesDoFormularioCorretas()
+        {
+            //Pegando os elementos e gerando valores de testes
+            var txtNome = this.Browser.FindElement(By.Id("Nome"));
+            var txtEmail = this.Browser.FindElement(By.Id("Email"));
+            var txtPassword = this.Browser.FindElement(By.Id("Password"));
+
+            //Envia os dados para o formulário
+            txtNome.SendKeys("Rafael Cruz");
+            txtEmail.SendKeys("teste@teste.com.br");
+            txtPassword.SendKeys("123Mudar");
+        }
+        
+        [When(@"clicar no botão de criar conta")]
+        public void QuandoClicarNoBotaoDeCriarConta()
+        {
+            var btnCriarConta = this.Browser.FindElement(By.Id("btnSubmit"));
+
+            //Faz o submit no formulario
+            btnCriarConta.Submit();
+
+        }
+        
+        [When(@"não inseri as informações de email")]
+        public void QuandoNaoInseriAsInformacoesDeEmail()
+        {
+            //Pegando os elementos e gerando valores de testes
+            var txtNome = this.Browser.FindElement(By.Id("Nome"));
+            var txtPassword = this.Browser.FindElement(By.Id("Password"));
+
+            //Envia os dados para o formulário
+            txtNome.SendKeys("Rafael Cruz");
+            txtPassword.SendKeys("123Mudar");
+        }
+        
+        [Then(@"o usuário deve ser redirecionado para a Home Page")]
+        public void EntaoOUsuarioDeveSerRedirecionadoParaAHomePage()
+        {
+            Assert.IsTrue(this.Browser.Title == "Home Page - My ASP.NET MVC Application");
+        }
+        
+        [Then(@"a pagina de cadastro deve exibir uma mensagem de erro")]
+        public void EntaoAPaginaDeCadastroDeveExibirUmaMensagemDeErro()
+        {
+            Assert.IsTrue(this.Browser.FindElement(By.XPath("//span[@data-valmsg-for='Email']")).Text == "Email é obrigatório");
+        }
+    }
+}
--- BDD/BDD/Properties/AssemblyInfo.cs
+++ BDD/BDD/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BDD")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bda8163a-9597-45d1-967a-fa8804467d08")]
+[assembly: Guid("3cb2122e-b080-4c44-9a15-f90cd4240aed")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
